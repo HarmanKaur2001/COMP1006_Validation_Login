8d1ec7147261547298c79f83597be0eb7f6adc5a
HarmanKaur2001 <200456178@student.georgianc.on.ca>
2021-04-09 21:34:46 -0400

    add validation

--- connect.php
+++ connect.php
@@ -2,12 +2,20 @@
 try {
     //try to connect to database 
     //data source name 
-    $dsn = 'mysql:host=localhost;dbname=COMP1006_Winter2021Sem';
-    $username = 'root'; 
-    $password = '';
+   // $dsn = 'mysql:host=localhost;dbname=COMP1006_Winter2021Sem';
+   // $username = 'root'; 
+    //$password = '';
     //create instance of PDO object
+
+    $dsn = 'mysql:host=172.31.22.43 ;dbname=Harman200456178';
+    $username = 'Harman200456178'; 
+    $password = 'x3lXI_p4uv';
     $db = new PDO($dsn,$username, $password); 
-    //echo 'Connected successfully! Whoo hoo!!!!'; 
+    //echo 'Connected successfully! Whoo hoo!!!!';
+    
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $db;
 }
 catch(PDOException $e ) {
     //display error message if things go wrong! 
